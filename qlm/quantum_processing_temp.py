d234441d93098c0228597fb7e123297a47c752f5
Asad Jamal <166385514+AsadCognify@users.noreply.github.com>
2024-07-08 19:29:56 +0500

    Update bug fix for displaying batch size. Error in quotations and f-string

--- qlm/quantum_processing_temp.py
+++ qlm/quantum_processing_temp.py
@@ -90,7 +90,7 @@ class LLAMA3:
             
             # modify batch size if necessary
             config["batch_size"] = min(config["batch_size"], len(training_dataset))
-            logger.info(f"batch size: {config["batch_size"]}")
+            logger.info(f"batch size: {config['batch_size']}")
 
             # laod model and tokenizer
             logger.debug(f"Loading model and tokenizer from {config['model_dir']}")
