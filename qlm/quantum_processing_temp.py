5d484bd3703c79a00fede67ae949767c196b7ea1
AsadCognify <asad@cognify-ai.com>
2024-07-11 15:44:30 +0500

    Re-added post finetune cleanup

--- qlm/quantum_processing_temp.py
+++ qlm/quantum_processing_temp.py
@@ -128,9 +128,9 @@ class LLAMA3:
                 eval_loss_df = pd.read_csv(path_to_loss_file)
                 cleaned_losses = eval_loss_df['eval_loss'].dropna()
 
-                logger.info(f"Cleaning up folder: {config['out_path']} - skipped.")
+                logger.info(f"Cleaning up folder: {config['out_path']}")
                 # os.removedirs(config["out_path"])
-                # shutil.rmtree(config["out_path"])
+                shutil.rmtree(config["out_path"])
 
                 # Update mongo status
                 LLAMA3._update_mongo(status='complete', loss=cleaned_losses.iloc[-1], bot_endpoint=f'{params["definition"]["bot_id"]}/{params["definition"]["combination_id"]}')
