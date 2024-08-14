65084b08ba990184e0d3d329aeaad652893a9f83
AsadCognify <asad@cognify-ai.com>
2024-08-14 17:33:51 +0500

    Resolving 422 Unprocessible Entity Error: notification code has to be sent as a string.

--- qlm/bayesian/bayesian.py
+++ qlm/bayesian/bayesian.py
@@ -119,7 +119,7 @@ def bayesian_finetune(data, ip_port:str=None, pod_id:Dict=None):
             if data['testing']['state'] == True:
                     request_terminate = requests.post(
                     url=f"{data['testing']['url']}/notify",
-                    json={"token": data["token"], "code": 36, "bot_endpoint": ""}
+                    json={"token": data["token"], "code": "36", "bot_endpoint": ""}
                 )
             
 
